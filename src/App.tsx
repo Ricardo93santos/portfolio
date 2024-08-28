07164ac099d5210087974f2a1261d44d5e27fb5e
studio <studioworkart24@gmail.com>
2024-08-28 14:15:57 -0300

    removendo logo onde não deveria estar.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import './App.css';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
-import logo from './logo.png';
 
 function App() {
   return (
     <>
-      <img src={logo} alt='Logo'></img>
       <Header />
       <Main />
       <Footer />
