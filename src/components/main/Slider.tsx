9b86a95c65b37e0ee681979d52a611ec25591410
studio <studioworkart24@gmail.com>
2024-09-09 20:05:01 -0300

    Alteração no componente para telas menores que 768px ficar com 6 itens

--- src/components/main/Slider.tsx
+++ src/components/main/Slider.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { technologies } from "./DadaTechnologies";
 import "./Slider.css";
 
 const Slider: React.FC = () => {
+
+  const [filteredTechnologies, setFilteredTechnologies] = useState<TechnologiesProps[]>([]);
 
   type TechnologiesProps = {
     src: string;
     alt: string;
   };
 
-  const listTechnologies = technologies.map((techtnologies: TechnologiesProps, index) => (
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setFilteredTechnologies(technologies);
+      } else {
+        setFilteredTechnologies(technologies.slice(0, 6));
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Chama a função uma vez para definir o estado inicial
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const listTechnologies = filteredTechnologies.map((techtnologies: TechnologiesProps, index) => (
     <li className="sliderlItens" style={{animationDelay: `${index * 1.4}s`}} key={index}>
       <img src={techtnologies.src} alt={techtnologies.alt} />
     </li>
