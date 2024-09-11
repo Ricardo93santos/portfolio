e74b75b99a9d8f12366ae00a08acb5c0eecc37e1
studio <studioworkart24@gmail.com>
2024-09-11 11:23:12 -0300

    Conclusão do slide

--- src/components/main/DadaTechnologies.tsx
+++ src/components/main/DadaTechnologies.tsx
@@ -1,9 +1,12 @@
 export const technologies = [
   { src: "https://img.icons8.com/?size=100&id=108784&format=png&color=000000", alt: "JavaScript" },
-  { src: "https://img.icons8.com/?size=100&id=wPohyHO_qO1a&format=png&color=000000", alt: "React" },
+  { src: "https://img.icons8.com/?size=100&id=t4YbEbA834uH&format=png&color=000000", alt: "React" },
   { src: "https://img.icons8.com/?size=100&id=uJM6fQYqDaZK&format=png&color=000000", alt: "TypeScript" },
+  { src: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxZW0iIGhlaWdodD0iMWVtIiB2aWV3Qm94PSIwIDAgMjQgMjQiPjxwYXRoIGZpbGw9IiMzY2M2MzkiIGQ9Ik00IDJoMTAuNWE2IDYgMCAwIDEgMy4wNTQgMTEuMTY2QTYgNiAwIDAgMSAyMCAxOHYzaC0ydi0zYTQgNCAwIDAgMC00LTRINHYtMmgxMC41YTQgNCAwIDAgMCAwLThINHptNyAxOUg0di0yaDd6Ii8+PC9zdmc+", alt: "Remix" },
   { src: "https://img.icons8.com/?size=100&id=EAUyKy3IwmqM&format=png&color=000000", alt: "HTML5" },
   { src: "https://img.icons8.com/?size=100&id=21278&format=png&color=000000", alt: "CSS3" },
-  { src: "https://img.icons8.com/?size=100&id=fAMVO_fuoOuC&format=png&color=000000", alt: "PHP" },
+  { src: "https://img.icons8.com/?size=100&id=hsPbhkOH4FMe&format=png&color=000000", alt: "Node.JS" },
+  { src: "https://img.icons8.com/?size=100&id=CIAZz2CYc6Kc&format=png&color=000000", alt: "TailwindCSS" },
+  { src: "https://img.icons8.com/?size=100&id=UGYn5TapNioV&format=png&color=000000", alt: "PHP" },
   { src: "https://img.icons8.com/?size=100&id=UFXRpPFebwa2&format=png&color=000000", alt: "MySQL" },
 ]; 
