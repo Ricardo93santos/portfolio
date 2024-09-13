67994a577a14f2e33328e1968cab57248734aea1
studio <studioworkart24@gmail.com>
2024-09-13 19:04:36 -0300

    Correção de bug no home

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -8,14 +8,12 @@ const Home: React.FC = () => {
         <h1>Bem-vindo a um novo universo digital!</h1>
         <h2>Eu sou Ricardo Santos</h2>
         <p>Transformo suas ideias em soluções web que fazem a diferença.</p>
-        <p>Entre em contato e vamos começar!</p>
         <div className="home_contact">
           <a href="https://wa.me/+5577933016666?text=Olá%20Ricardo,%20gostaria%20de%20saber%20mais%20sobre%20seus%20serviços!%20Vamos%20criar%20um%20site?" target="_blank" rel="noopener noreferrer">
-            <button className="whatsappButton" aria-label="Fale Conosco no WhatsApp">Fale Conosco no WhatsApp</button>
+            <button className="whatsappButton" aria-label="Me chama no WhatsApp">Me chama no WhatsApp</button>
           </a>
-          <p>ou</p>
           <a href="./contact">
-            <button className="contactButton" aria-label="Preencha o Formulário de Contato">Preencha o Formulário</button>
+            <button className="contactButton" aria-label="Preencha o Formulário">Preencha o Formulário</button>
           </a>
         </div>
       </div>
