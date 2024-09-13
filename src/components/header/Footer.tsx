3bd095ec0e0346a768db5aaeea294780a744061b
studio <studioworkart24@gmail.com>
2024-09-13 16:11:41 -0300

    Troca do fontawesome para Icon.

--- src/components/header/Footer.tsx
+++ src/components/header/Footer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faWhatsapp, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { Icon } from '@iconify-icon/react';
 import './Footer.css';
 
 interface FooterProps{
@@ -13,14 +12,19 @@ const Footer: React.FC<FooterProps> = ({ expanded }) => {
       <ul className={expanded ? 'FooterExpanded' : 'FooterCollapsed'}>
         <li>
           <a href="https://github.com/Ricardo93santos" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faGithub} />
+          <Icon icon="mdi:github" />
           </a>
         </li>
-        <li><a href="https://www.linkedin.com/in/ricardo-santos-997429145/" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faLinkedin} />
-          </a></li>
-        <li><a href="https://wa.me/5577933016666" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faWhatsapp} /></a></li>
+        <li>
+          <a href="https://www.linkedin.com/in/ricardo-santos-997429145/" target="_blank" rel="noopener noreferrer">
+            <Icon icon="mdi:linkedin" />
+          </a>
+        </li>
+        <li>
+          <a href="https://wa.me/5577933016666" target="_blank" rel="noopener noreferrer">
+            <Icon icon="ic:baseline-whatsapp" />
+          </a>
+        </li>
       </ul>
     </footer>
   )
