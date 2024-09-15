2e8d4f76f2ef6342815ff4ff823a55869d6645e2
studio <studioworkart24@gmail.com>
2024-09-15 01:28:05 -0300

    Melhorando o texto de about

--- src/components/main/About.tsx
+++ src/components/main/About.tsx
@@ -7,9 +7,8 @@ const About: React.FC = () => {
     <section className="container about">
       <div className="about_text">
         <h2>Me conheça</h2>
-        <p>Sou formado em Análise e Desenvolvimento de Sistemas, com especialização em desenvolvimento web focado no front-end. </p>
-        <p>Minha paixão é transformar ideias em interfaces intuitivas e funcionais, sempre buscando a melhor experiência para o usuário.</p>
-        <p>Tenho experiência no desenvolvimento de sistemas web, tanto como freelancer quanto em projetos pessoais, utilizando as linguagens e frameworks mais modernos.</p>
+        <p>Sou formado em Análise e Desenvolvimento de Sistemas, com especialização em desenvolvimento web focado no front-end.</p>
+        <p>Meu objetivo é transformar ideias em interfaces intuitivas e funcionais, sempre buscando proporcionar a melhor experiência para o usuário.</p>        
       </div>
       <Slider />
     </section>
