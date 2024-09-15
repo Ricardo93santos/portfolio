1aa53b3165ffd863f7060c1d3d851f7631429133
studio <studioworkart24@gmail.com>
2024-09-15 01:16:00 -0300

    Adição do onInput para impedir digitar outra coisa sem ser número no campo tel

--- src/components/main/Forms.tsx
+++ src/components/main/Forms.tsx
@@ -2,20 +2,26 @@ import React from "react";
 import "../../style/Forms.css";
 
 const Forms: React.FC = () => {
+
+  const handleInput = (event: React.FormEvent<HTMLInputElement>) => {
+    const input = event.currentTarget;
+    input.value = input.value.replace(/[^0-9]/g, '');
+  };
+
   return (
-    <form className="containerForms" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" >
+    <form className="containerForms" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
       <input type="hidden" name="form-name" value="contact" />
       <label htmlFor="name">Nome</label>
       <input type="text" name="name" placeholder="Nome completo" required />
       <label htmlFor="email">E-mail</label>
       <input type="email" name="email" placeholder="E-mail@email.com" required />
       <label htmlFor="phone">Telefone ou WhatsApp</label>
-      <input type="tel" name="phone" placeholder="(xx)9xxxx-xxxx" minLength={11} required />
+      <input type="tel" name="phone" placeholder="(xx)9xxxx-xxxx" onInput={handleInput} minLength={11} required />
       <label htmlFor="message">Deixe sua mensagem</label>
       <textarea name="message" placeholder="Fique à vontade" required></textarea>
       <button type="submit">Enviar</button>
     </form>
-  )
-}
+  );
+};
 
 export default Forms;
