fa4a407d46b8cdc14a671bd96482dbb9b71bbf43
studio <studioworkart24@gmail.com>
2024-09-15 00:54:10 -0300

    Colocoando required no form

--- src/components/main/Forms.tsx
+++ src/components/main/Forms.tsx
@@ -6,13 +6,13 @@ const Forms: React.FC = () => {
     <form className="containerForms" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" >
       <input type="hidden" name="form-name" value="contact" />
       <label htmlFor="name">Nome</label>
-      <input type="text" name="name" placeholder="Nome completo" />
+      <input type="text" name="name" placeholder="Nome completo" required />
       <label htmlFor="email">E-mail</label>
-      <input type="email" name="email" placeholder="E-mail@email.com" />
+      <input type="email" name="email" placeholder="E-mail@email.com" required />
       <label htmlFor="phone">Telefone ou WhatsApp</label>
-      <input type="tel" name="phone" placeholder="(xx)9xxxx-xxxx" />
+      <input type="tel" name="phone" placeholder="(xx)9xxxx-xxxx" minLength={11} required />
       <label htmlFor="message">Deixe sua mensagem</label>
-      <textarea name="message" placeholder="Fique à vontade"></textarea>
+      <textarea name="message" placeholder="Fique à vontade" required></textarea>
       <button type="submit">Enviar</button>
     </form>
   )
