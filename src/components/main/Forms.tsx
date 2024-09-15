a2db3e3dfbbd59affd230c48ed1187f04bcb0ff6
studio <studioworkart24@gmail.com>
2024-09-15 00:36:39 -0300

    Removendo a atulização do formulário ao enviar

--- src/components/main/Forms.tsx
+++ src/components/main/Forms.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import "../../style/Forms.css";
 
 const Forms: React.FC = () => {
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+  }
   return (
-    <form className="containerForms" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+    <form className="containerForms" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleSubmit}>
       <input type="hidden" name="form-name" value="contact" />
       <label htmlFor="name">Nome</label>
       <input type="text" name="name" placeholder="Nome completo" />
