06d20d77c9ead4ef3c91dfa33238b75bc70a1e5d
studio <studioworkart24@gmail.com>
2024-09-17 17:52:13 -0300

    Conclusão do carousel

--- src/components/main/Cards.tsx
+++ src/components/main/Cards.tsx
@@ -1,22 +1,45 @@
-  import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import '../../style/Cards.css';
 import LinksProjects from "./LinksProjects";
 import TechnologiesCards from "./TechnologiesCards";
 import ThumbProjects from "./ThumbProjects";
 import { projects, Project } from "./Dada";
+import { motion } from "framer-motion";
 
 const Cards: React.FC = () => {
 
+  const carousel = useRef<HTMLDivElement>(null);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    if (carousel.current) {
+      const scrollWidth = carousel.current.scrollWidth;
+      const offsetWidth = carousel.current.offsetWidth;
+
+      setWidth(scrollWidth - offsetWidth);
+      carousel.current.scrollLeft = 100;
+    }
+  }, []);
+
   return (
-    <div className="containerCards">
-      {projects.map((project: Project, index) => (
-        <div key={index} className="card">
-          <ThumbProjects src={project.src} alt={project.alt}/>
-          <LinksProjects link={project.link}/>
-          <TechnologiesCards technologies={project.technologies}/>
-        </div>
-      ))}
-    </div>
+    <motion.div ref={carousel} className="carouselCards" whileTap={{ cursor: "grabbing" }} >
+      <motion.div 
+        className="containerCards"
+        drag="x"
+        dragConstraints={{ right: 610, left: -width }}
+        initial={{ x: 200 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.8 }}
+        >
+        {projects.map((project: Project, index) => (
+          <motion.div key={index} className="card">
+            <ThumbProjects src={project.src} alt={project.alt}/>
+            <LinksProjects link={project.link}/>
+            <TechnologiesCards technologies={project.technologies}/>
+          </motion.div>
+        ))}
+      </motion.div>
+    </motion.div>
   );
 };
 
