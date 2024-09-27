499d22f97a7066fa189e7ea49575d12e179da3a2
studio <studioworkart24@gmail.com>
2024-09-27 14:11:00 -0300

    Melhora e conclusão do texto

--- src/components/main/About.tsx
+++ src/components/main/About.tsx
@@ -6,9 +6,9 @@ const About: React.FC = () => {
   return (
     <section className="container about">
       <div className="about_text">
-        <p>Analista e desenvolvedor de sistemas especializado em desenvolvimento web, com foco no front-end. Transformo ideias em interfaces intuitivas e atraentes, sempre buscando a melhor experiência para o usuário.</p>
-        <p>Trabalho com startups, projetos freelance e outras iniciativas, sempre me capacitando e atualizando para entregar sistemas web modernos e funcionais.</p>        
-        <p>Meu objetivo é transformar ideias em interfaces intuitivas e funcionais, sempre buscando proporcionar a melhor experiência para o usuário. Se você procura alguém comprometido e entusiasmado para criar soluções incríveis, vamos conversar!</p>        
+        <p>Analista e desenvolvedor de sistemas especializado em desenvolvimento web, com foco no front-end. Transformo ideias em interfaces intuitivas e atraentes.</p>
+        <p>Trabalho com startups, projetos freelance e outras iniciativas, sempre me capacitando e atualizando para entregar sistemas web modernos e funcionais. Meu objetivo é transformar ideias em interfaces intuitivas, sempre buscando proporcionar a melhor experiência para o usuário.</p>        
+        <p>Se você procura alguém comprometido e entusiasmado para criar soluções incríveis, vamos conversar!</p>        
       </div>
       <Slider />
     </section>
