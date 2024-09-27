cd2444771ef3dc7f4aa741a3c9f03155830721fa
studio <studioworkart24@gmail.com>
2024-09-27 14:19:36 -0300

    Removendo texto que vai para o home, reposicionando o Slide de tecnologias para mehlorar a interface.

--- src/components/main/About.tsx
+++ src/components/main/About.tsx
@@ -5,12 +5,12 @@ import "../../style/About.css";
 const About: React.FC = () => {
   return (
     <section className="container about">
+      <Slider />
       <div className="about_text">
-        <p>Analista e desenvolvedor de sistemas especializado em desenvolvimento web, com foco no front-end. Transformo ideias em interfaces intuitivas e atraentes.</p>
+        <p>Analista e desenvolvedor de sistemas especializado em desenvolvimento web, com foco no front-end.</p>
         <p>Trabalho com startups, projetos freelance e outras iniciativas, sempre me capacitando e atualizando para entregar sistemas web modernos e funcionais. Meu objetivo é transformar ideias em interfaces intuitivas, sempre buscando proporcionar a melhor experiência para o usuário.</p>        
         <p>Se você procura alguém comprometido e entusiasmado para criar soluções incríveis, vamos conversar!</p>        
       </div>
-      <Slider />
     </section>
   );
 }
