0bd30bf7a649bc4e1033f1655f9d399daf4800ed
studio <studioworkart24@gmail.com>
2024-09-27 14:24:16 -0300

    Atualizando tag de a para component Link, deixando mais rápida a navegação.

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../style/Home.css";
+import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
@@ -9,12 +10,12 @@ const Home: React.FC = () => {
         <h2>Eu sou Ricardo Santos</h2>
         <p>Transformo ideias em interfaces intuitivas e atraentes.</p>
         <div className="home_contact">
-          <a href="https://wa.me/+5577933016666?text=Olá%20Ricardo,%20gostaria%20de%20saber%20mais%20sobre%20seus%20serviços!%20Vamos%20criar%20um%20site?" target="_blank" rel="noopener noreferrer">
+          <Link to="https://wa.me/+5577933016666?text=Olá%20Ricardo,%20gostaria%20de%20saber%20mais%20sobre%20seus%20serviços!%20Vamos%20criar%20um%20site?" target="_blank" rel="noopener noreferrer">
             <button className="whatsappButton" aria-label="Me chama no WhatsApp">Me chama no WhatsApp</button>
-          </a>
-          <a href="/contact">
+          </Link>
+          <Link to="/contact">
             <button className="contactButton" aria-label="Preencha o Formulário">Preencha o Formulário</button>
-          </a>
+          </Link>
         </div>
       </div>
       <img className="profileImage" src="https://media.licdn.com/dms/image/v2/D4D03AQHynDl0BNBfGQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1686515034641?e=1730937600&v=beta&t=xFsr6H55cSsN2UjwDFvmqzDY8Pbjmydt1jWsDLGYvcA" alt="Foto do Perfil" />
