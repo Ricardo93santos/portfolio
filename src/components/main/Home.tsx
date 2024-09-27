bd3a046a00bcfb2eea7666bca7eeb655662bf9a1
studio <studioworkart24@gmail.com>
2024-09-27 14:20:34 -0300

    Alterando texto do home, deixando mais atrativo e pessoal.

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
       <div className="home_text">
         <h1>Bem-vindo a um novo universo digital!</h1>
         <h2>Eu sou Ricardo Santos</h2>
-        <p>Transformo suas ideias em soluções web que fazem a diferença.</p>
+        <p>Transformo ideias em interfaces intuitivas e atraentes.</p>
         <div className="home_contact">
           <a href="https://wa.me/+5577933016666?text=Olá%20Ricardo,%20gostaria%20de%20saber%20mais%20sobre%20seus%20serviços!%20Vamos%20criar%20um%20site?" target="_blank" rel="noopener noreferrer">
             <button className="whatsappButton" aria-label="Me chama no WhatsApp">Me chama no WhatsApp</button>
